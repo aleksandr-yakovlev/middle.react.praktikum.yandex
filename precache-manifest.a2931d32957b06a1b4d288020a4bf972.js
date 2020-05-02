51048ab2935a1e14a70a493cc1822059308c9696
Aleksandr Yakovlev <Aleksandr.Yakovlev@live.com>
2020-05-02 14:43:07 +0300

    Updates

---
+++ precache-manifest.a2931d32957b06a1b4d288020a4bf972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407c3e53365fee9cd0bc842349925b40",
+    "url": "/bubble/index.html"
+  },
+  {
+    "revision": "7e942ab10d2e22ffd995",
+    "url": "/bubble/static/css/main.a7dad95f.chunk.css"
+  },
+  {
+    "revision": "123522f6030122f9318f",
+    "url": "/bubble/static/js/2.e3cbda89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bubble/static/js/2.e3cbda89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e942ab10d2e22ffd995",
+    "url": "/bubble/static/js/main.92cfc2cb.chunk.js"
+  },
+  {
+    "revision": "92d89731edc5d4bbcf46",
+    "url": "/bubble/static/js/runtime-main.996b95e0.js"
+  }
+]);
